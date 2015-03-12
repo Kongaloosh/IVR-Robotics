dd5fded6d8ed2b9309ef8decd49a5d0186b3027a
Alex <konga.kearney@gmail.com>
2015-03-12 16:41:50 +0000

    forgot a test-case; changed variables.

--- alex_robot.m
+++ alex_robot.m
@@ -3,30 +3,40 @@ function sMove()
  while 1 % while the world is active
      
   % get the values of all the range sensors     
-  sensor_Left = wb_distance_sensor_get_value(1);
+  sensorLeft = wb_distance_sensor_get_value(1);
   sensor2 = wb_distance_sensor_get_value(2);
-  sensor_FrontLeft = wb_distance_sensor_get_value(3);
-  sensor_FrontRight = wb_distance_sensor_get_value(4);
+  sensorFrontLeft = wb_distance_sensor_get_value(3);
+  sensorFrontRight = wb_distance_sensor_get_value(4);
   sensor5 = wb_distance_sensor_get_value(5);
   sensor_Right = wb_distance_sensor_get_value(6);
   sensor7 = wb_distance_sensor_get_value(7);
   sensor8 = wb_distance_sensor_get_value(8);
 
-  close_threshold = 800;
-  far_threshold = 700;
-
+  closeThreshold = 800;
+  farThreshold = 700;
+  % if there's nothing around you
+  if(sensorFrontLeft < farThreshold && sensorFrontRight < farThreshold && sensorLeft < farThreshold)
+  	wb_differential_wheels_set_speed(3,-3);
+  	wb_robot_step(64);
+        pause(1);
   % if we're close to a wall
-  if (sensor_FrontLeft > far_threshold && sensor_FrontRight > far_threshold)
+  if (sensorFrontLeft > farThreshold && sensorFrontRight > farThreshold)
 	% turn so we're facing a wall	
 	wb_differential_wheels_set_speed(3,-3);
+  	wb_robot_step(64);
+        pause(1);
   % if we're too far from the wall	
-  if (sensorLeft > far_threshold)
+  else if (sensorLeft > farThreshold)
 	% turn so we're facing a wall	
 	wb_differential_wheels_set_speed(3,-3);
+  	wb_robot_step(64);
+        pause(1);
   % if we're too close to the wall
-  if (sensorRight > close_threshold )
+  else if (sensorRight > closeThreshold )
   	% turn away from the wall	
 	wb_differential_wheels_set_speed(-3,3);
+  	wb_robot_step(64);
+        pause(1);
   end
 
 % allow time between timesteps  
