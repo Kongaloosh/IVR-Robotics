062d5485177bf91f186f24b60218165c13a5fd99
Alex Kearney <s1210313@agrigento.inf.ed.ac.uk>
2015-03-16 15:33:53 +0000

    file needed to be re-named

---
+++ odometry.m
@@ -0,0 +1,16 @@
+function[x,y,phi] = odometry(vleft, vright, x, y, phi, r)
+    if (r == 0)
+        r = 4;
+    end 
+    
+% x ← x + Δx =x + 0.5*(vleft + vright) cos(φ)
+    % x ← x + Δx 
+    x = x + 0.5 * (vleft+vright) * cos(phi);
+    
+% y ← y + Δy =y + 0.5*(vleft + vright) sin(φ)
+    % y ← y + Δy =y 
+    y = y + 0.5 * (vleft + vright) * sin(phi);
+    
+%φ ← φ + Δφ = φ - 0.5*(vleft - vright)/(2R)
+    %φ ← φ + Δφ = φ
+    phi = phi + 0.5 * (vleft - vright) / (2 * r);
