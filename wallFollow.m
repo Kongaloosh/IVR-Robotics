465756e7f91acb31d5945dbc4f57a3a100771e7f
SaltedCashew <a10gac@gmail.com>
2015-03-17 21:07:38 +0000

    updates to wallFollowing. seems to be working much better.

--- wallFollow.m
+++ wallFollow.m
@@ -2,6 +2,7 @@ function wallFollow()
 
 disp('Starting now!');
 
+%%Get an initial reading
 sensorStartLeftBack = wb_distance_sensor_get_value(1);
 sensorStartRightBack = wb_distance_sensor_get_value(6);
 sensorStartFrontLeft = wb_distance_sensor_get_value(3);
@@ -17,13 +18,15 @@ leftClose = 600;
 leftFar = 400;
 noDetection = 0;
 close = 500;
+
+%define default speed
+normSpd = 3;
+reverseNormSpd = -3;
   
-  
-%not sensing anything, so floor it
+%Just starting off, if not sensing anything, floor it
 while(sensorStartLeftBack + sensorStartRightBack + sensorStartFrontLeft + sensorStartFrontRight < 50)
     disp('Moving forward');
-    disp(strcat('Front Left:',{' '}, num2str(sensorFrontLeft)));
-  	wb_differential_wheels_set_speed(8,8);
+   	wb_differential_wheels_set_speed(normSpd+5,normSpd+5);
   	wb_robot_step(64);
     sensorStartLeftBack = wb_distance_sensor_get_value(1);
     sensorStartFrontLeft = wb_distance_sensor_get_value(3);
@@ -34,7 +37,7 @@ end
 
 disp('Leaving first while loop');
 
-while 1 % while the world is active
+while 1 %bot has approached something. while world is active, keep looping
  
   % get the values of all the range sensors     
   sensorLeftBack = wb_distance_sensor_get_value(1);
@@ -45,7 +48,7 @@ while 1 % while the world is active
   sensorRightBack = wb_distance_sensor_get_value(6);
   sensorBackRight = wb_distance_sensor_get_value(7);
   sensorBackLeft = wb_distance_sensor_get_value(8);
-
+  wb_robot_step(64); %%needed here or the sensors won't read correctly!
   
 
   
@@ -54,7 +57,7 @@ while 1 % while the world is active
   if(sensorFrontLeft == noDetection && sensorFrontRight == noDetection ...
           && sensorLeftBack < leftClose && sensorLeftBack > leftFar ...
           && sensorLeftForward < tooClose && sensorRightForward < tooClose)
-    wb_differential_wheels_set_speed(3,3);
+    wb_differential_wheels_set_speed(normSpd,normSpd);
   	wb_robot_step(64);
     disp('forward');
     followFlag = 1;
@@ -63,24 +66,24 @@ while 1 % while the world is active
   %nothing is detected all around, so go forward quickly
   elseif(sensorFrontLeft == noDetection && sensorFrontRight == noDetection ...
          && sensorLeftBack == noDetection && sensorRightBack == noDetection)
-%     if(followFlag == 1)
-%         disp('Huh? Was just following something, now its gone!')  
-%         wb_differential_wheels_set_speed(-3,3);
-%         wb_robot_step(64);
-%         pause(.5)
-%         followFlag = 0;
-%     else
+    if(followFlag == 1)
+        disp('Huh? Was just following something, now its gone!')  
+        wb_differential_wheels_set_speed(-3,3);
+        wb_robot_step(64);
+        pause(.5)
+        followFlag = 0;
+    else
         disp('Moving forward');
-        wb_differential_wheels_set_speed(3,3);
+        wb_differential_wheels_set_speed(normSpd, normSpd);
         wb_robot_step(64);
         followFlag = 0;
-%     end
+     end
 
   %nothing in front & left wall is sensed but too far away
   %getting further away from wall than desired, so go forward and left
   elseif(sensorFrontLeft == noDetection && sensorFrontRight == noDetection ...
           && sensorLeftBack < leftFar && sensorLeftBack > noDetection)
-    wb_differential_wheels_set_speed(2,3);
+    wb_differential_wheels_set_speed(normSpd - ((leftFar-sensorLeftBack)/100), normSpd);
   	wb_robot_step(64);
     disp('left forward');
     followFlag = 1;
@@ -90,18 +93,15 @@ while 1 % while the world is active
   elseif(sensorFrontLeft == noDetection && sensorFrontRight == noDetection ...
           && sensorLeftBack > leftClose && sensorLeftBack<tooClose ...
           && sensorLeftForward <tooClose)
-     wb_differential_wheels_set_speed(3,2);
+     wb_differential_wheels_set_speed(normSpd,normSpd - 1);
   	wb_robot_step(64);
      disp('right forward');
      followFlag = 1;
    
-   %nothing in front, but left wal is too close
+   %nothing in front, but left wall is too close
    %back up a bit, then turn slightly
    elseif(sensorFrontLeft == noDetection && sensorFrontRight == noDetection ...
               &&  sensorLeftBack > tooClose)
-    wb_differential_wheels_set_speed(-2,-2);
-  	wb_robot_step(64);
-    pause(1);   
     wb_differential_wheels_set_speed(2,-2);
   	wb_robot_step(64);
     pause(.4);
@@ -112,15 +112,12 @@ while 1 % while the world is active
   %currently same actions as previous elseif statement
   elseif(sensorFrontLeft > tooClose || sensorFrontRight > tooClose ...
           || sensorLeftForward > close || sensorRightForward > close)
-    wb_differential_wheels_set_speed(-3,-3);
-  	wb_robot_step(64);
-    pause(1);   
-    wb_differential_wheels_set_speed(3,-3);
+    wb_differential_wheels_set_speed(normSpd, reverseNormSpd);
   	wb_robot_step(64);
     pause(.3);
     disp('back then spin round');
     followFlag = 1;
-  else disp ('DANGER WILL ROBINSON!');  
+  else disp ('DANGER WILL ROBINSON!');  %%in a situation not accounted for
         disp(wb_distance_sensor_get_value(4))
   end
   
